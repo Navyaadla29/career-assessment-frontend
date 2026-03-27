09645768206c63dea2964418a9d816dda4826508
AdlaNavya <adlanavya99@gmail.com>
2026-03-27 15:18:20 +0530

    Removed custom cursor and particles - fixed cursor issue

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Module3_Personality from "./pages/Module3_Personality";
 import Module4_Values from "./pages/Module4_Values";
 import Module5_WorkStyle from "./pages/Module5_WorkStyle";
 import Module6_AcademicTrack from "./pages/Module6_AcademicTrack";
-import Module7_FutureGoals from "./pages/Module7_FutureGoals";  // ADD THIS
+import Module7_FutureGoals from "./pages/Module7_FutureGoals";
 import Scoring from "./pages/Scoring";
 import CareerMatch from "./pages/CareerMatch";
 import StreamRecommendation from "./pages/StreamRecommendation";
@@ -33,7 +33,7 @@ function App() {
         <Route path="/module4" element={<Module4_Values />} />
         <Route path="/module5" element={<Module5_WorkStyle />} />
         <Route path="/module6" element={<Module6_AcademicTrack />} />
-        <Route path="/module7" element={<Module7_FutureGoals />} />  {/* ADD THIS */}
+        <Route path="/module7" element={<Module7_FutureGoals />} />
         <Route path="/scoring" element={<Scoring />} />
         <Route path="/career-match" element={<CareerMatch />} />
         <Route path="/stream-recommendation" element={<StreamRecommendation />} />
